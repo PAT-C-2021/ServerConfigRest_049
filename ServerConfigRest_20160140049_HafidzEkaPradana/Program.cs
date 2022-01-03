b4086331766b81cec58fa2c0ec19e9c5eb53abde
HafidzEkaPradana <hafidz.eka.2016@ft.umy.ac.id>
2022-01-03 13:17:58 +0700

    Add project files.

---
+++ ServerConfigRest_20160140049_HafidzEkaPradana/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20160140049_HafidzEkaPradana;
+
+namespace ServerConfigRest_20160140049_HafidzEkaPradana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch(Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
